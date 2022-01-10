46bf5e84dd830584e14671322e262a99958e73f1
HyoungUkJJang <guddnr1451@gmail.com>
2022-01-11 00:40:53 +0900

    [ADD] - CI/CD 구축을 해보자 / 서버 체크 컨트롤라 추가작성

--- src/test/java/com/numble/karrot/KarrotApplicationTests.java
+++ src/test/java/com/numble/karrot/KarrotApplicationTests.java
@@ -3,11 +3,6 @@ package com.numble.karrot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class KarrotApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }

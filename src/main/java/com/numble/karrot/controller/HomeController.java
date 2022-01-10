90a034fe80ac46117e2b92a465642c5d94270b80
HyoungUkJJang <guddnr1451@gmail.com>
2022-01-11 00:37:49 +0900

    [ADD] - CI/CD 구축을 해보자 / 서버 체크 컨트롤라 추가작성

---
+++ src/main/java/com/numble/karrot/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.numble.karrot.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/health")
+public class HomeController {
+
+    @GetMapping
+    public String health() {
+        return "alive!";
+    }
+
+}

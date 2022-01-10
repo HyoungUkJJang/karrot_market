5f9da3ca4b391e710cb87297498b0afffefdc0d4
HyoungUkJJang <guddnr1451@gmail.com>
2022-01-10 16:33:12 +0900

    [ADD] - 개발환경 초기 세팅을 구성하자

---
+++ src/main/java/com/numble/karrot/KarrotApplication.java
@@ -0,0 +1,13 @@
+package com.numble.karrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarrotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarrotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numble/karrot/KarrotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble.karrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

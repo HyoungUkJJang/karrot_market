0c44b6263e7fcfdb4cc867cd048a7f3cd28138bb
HyoungUkJJang <guddnr1451@gmail.com>
2022-01-11 15:52:08 +0900

    [ADD] - CI/CD 자동배포 테스트7

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,6 +27,12 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 
 echo "> Jar Name: $JAR_NAME"
 
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
 nohup java -jar \
   -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-real-db.yml,classpath:/application-real.yml \
   -Dspring.profiles.active=real \

41f8fa4aa2fadff3c56a20053a94ade277341027
HyoungUkJJang <guddnr1451@gmail.com>
2022-01-19 22:58:21 +0900

    [ADD] - CROS 추가

--- src/main/java/com/numble/karrot/controller/ProductController.java
+++ src/main/java/com/numble/karrot/controller/ProductController.java
@@ -18,10 +18,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -32,6 +29,7 @@ import java.util.stream.Collectors;
  * 상품과 관련된 HTTP 요청을 처리하는 컨트롤러 클래스입니다.
  */
 @Controller
+@CrossOrigin
 @RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {

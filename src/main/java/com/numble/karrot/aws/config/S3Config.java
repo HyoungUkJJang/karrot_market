4a11e4df2021e46b4257b9a91ae2bde74de7d5c8
HyoungUkJJang <guddnr1451@gmail.com>
2022-01-20 14:49:59 +0900

    [update] - ec2 s3수정작업중

--- src/main/java/com/numble/karrot/aws/config/S3Config.java
+++ src/main/java/com/numble/karrot/aws/config/S3Config.java
@@ -12,27 +12,27 @@ import org.springframework.context.annotation.Configuration;
 /**
  * S3 설정 클래스입니다.
  */
-@Configuration
+//@Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
-    @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder
-                .standard()
-                .withRegion(region)
-                .withCredentials(
-                      new AWSStaticCredentialsProvider(awsCreds))
-                .build();
-    }
+//    @Value("${cloud.aws.credentials.accessKey}")
+//    private String accessKey;
+//
+//    @Value("${cloud.aws.credentials.secretKey}")
+//    private String secretKey;
+//
+//    @Value("${cloud.aws.region.static}")
+//    private String region;
+//
+//    @Bean
+//    public AmazonS3Client amazonS3Client() {
+//        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+//        return (AmazonS3Client) AmazonS3ClientBuilder
+//                .standard()
+//                .withRegion(region)
+//                .withCredentials(
+//                      new AWSStaticCredentialsProvider(awsCreds))
+//                .build();
+//    }
 
 }
